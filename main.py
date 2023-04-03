e54a0d23804124a83464871f439638eab3f008ed
siddharthvoonna <99697284+siddharthvoonna@users.noreply.github.com>
2023-04-02 22:12:50 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,5 @@
+git config --global user.name "Siddharth Voonna"
+git config --global user.email siddharth.voonna3@gmail.com
+git Which Code 
+/snap/bin/code
+git config --global core.editor "code --wait"
